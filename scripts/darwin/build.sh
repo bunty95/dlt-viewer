882a9818f9b9393393b98c7b7c2e4bcbcc498531
bunty95 <146869867+bunty95@users.noreply.github.com>
2024-06-13 10:54:23 -0400

    Update build.sh
    added qt6

--- scripts/darwin/build.sh
+++ scripts/darwin/build.sh
@@ -13,11 +13,13 @@ cd "${BUILD_DIR}"
 
 if [[ $(uname -m) == 'arm64' ]]; then
   Qt5_DIR="/opt/homebrew/opt/qt@5"
+  Qt6_DIR="/opt/homebrew/opt/qt@6"
   echo "Build with cmake $(uname -m) $Qt5_DIR"
   qmake ../BuildDltViewer.pro
-#  cmake ..
+  make 
 else
-  Qt5_DIR="/usr/local/opt/qt"
+  Qt5_DIR="/usr/local/opt/qt@5"
+  Qt6_DIR="/usr/local/opt/qt@6"
   echo "Build with qmake $(uname -m) $Qt5_DIR"
   qmake ../BuildDltViewer.pro
   make
@@ -36,7 +38,7 @@ echo Build with CMake
 # https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFBundles/BundleTypes/BundleTypes.html
 cmake -G Ninja \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
-  -DCMAKE_PREFIX_PATH=${Qt5_DIR}/lib/cmake \
+  -DCMAKE_PREFIX_PATH=${Qt6_DIR}/lib/cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=12.7 \
   -DCMAKE_BUILD_TYPE=Release \
   -DDLT_USE_QT_RPATH=ON \
