647862988e8b14da306c38fb149489b473240235
bunty95 <146869867+bunty95@users.noreply.github.com>
2024-10-11 09:50:46 -0400

    install of qt6

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -6,11 +6,12 @@ pwd
 
 brew update
 brew install qt@5
-brew unlink qt@5 && brew link --force qt@5
+brew install qt
+brew unlink qt@5 && brew link --force qt
 #brew link --overwrite qt@5 --force --overwrite
 # https://github.com/Homebrew/homebrew-core/issues/8392
 # https://github.com/Homebrew/legacy-homebrew/issues/29938
-QT_VERSION=$(brew list --versions qt@5 | awk '{print $2}')
+QT_VERSION=$(brew list --versions qt | awk '{print $2}')
 echo "QT_VERSION=$QT_VERSION"
 if [[ -z "$QT_VERSION" ]]; then
     echo "QT_VERSION is empty"
@@ -22,6 +23,10 @@ echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.bash_profile
 export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/qt@5/lib/pkgconfig"
+echo 'export PATH="/opt/homebrew/opt/qt@6/bin:$PATH"' >> ~/.bash_profile
+export LDFLAGS="-L/opt/homebrew/opt/qt@6/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/qt@6/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/qt@6/lib/pkgconfig"
 source ~/.bash_profile
 
 uname -a
@@ -29,10 +34,15 @@ if [[ $(uname -m) == 'arm64' ]]; then
   echo "Run on MacOS arm64"
   sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
   sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
+  sudo ln -s -F "/opt/homebrew/Cellar/qt@6/${QT_VERSION}/mkspecs" /usr/local/mkspecs
+  sudo ln -s -F "/opt/homebrew/Cellar/qt@6/${QT_VERSION}/plugins" /usr/local/plugins
 else
   echo "Run on MacOS x86"
   sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
   sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
+  sudo ln -s "/usr/local/Cellar/qt@6/${QT_VERSION}/mkspecs" /usr/local/mkspecs
+  sudo ln -s "/usr/local/Cellar/qt@6/${QT_VERSION}/plugins" /usr/local/plugins
+  
 fi
 
 brew install ninja tree
