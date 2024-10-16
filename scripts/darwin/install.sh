9f092f18f355add9070d7925c448c996b357bf68
bunty95 <146869867+bunty95@users.noreply.github.com>
2024-10-16 19:25:05 -0400

    qt6 only

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -5,9 +5,8 @@ env
 pwd
 
 brew update
-brew install qt@5
 brew install qt
-brew unlink qt@5 && brew link --force qt
+brew unlink qt && brew link --force qt
 #brew link --overwrite qt@5 --force --overwrite
 # https://github.com/Homebrew/homebrew-core/issues/8392
 # https://github.com/Homebrew/legacy-homebrew/issues/29938
@@ -19,10 +18,6 @@ if [[ -z "$QT_VERSION" ]]; then
 fi
 
 # follow brew install suggestions
-echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.bash_profile
-export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
-export PKG_CONFIG_PATH="/opt/homebrew/opt/qt@5/lib/pkgconfig"
 echo 'export PATH="/opt/homebrew/opt/qt@6/bin:$PATH"' >> ~/.bash_profile
 export LDFLAGS="-L/opt/homebrew/opt/qt@6/lib"
 export CPPFLAGS="-I/opt/homebrew/opt/qt@6/include"
@@ -32,14 +27,10 @@ source ~/.bash_profile
 uname -a
 if [[ $(uname -m) == 'arm64' ]]; then
   echo "Run on MacOS arm64"
-  sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
-  sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
   sudo ln -s -F "/opt/homebrew/Cellar/qt@6/${QT_VERSION}/mkspecs" /usr/local/mkspecs
   sudo ln -s -F "/opt/homebrew/Cellar/qt@6/${QT_VERSION}/plugins" /usr/local/plugins
 else
   echo "Run on MacOS x86"
-  sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
-  sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
   sudo ln -s "/usr/local/Cellar/qt@6/${QT_VERSION}/mkspecs" /usr/local/mkspecs
   sudo ln -s "/usr/local/Cellar/qt@6/${QT_VERSION}/plugins" /usr/local/plugins
   
