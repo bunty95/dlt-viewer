c8f1eb4119070300eef03b6c4dd23828ada572c5
bunty95 <146869867+bunty95@users.noreply.github.com>
2024-12-18 08:22:26 -0500

    qt6

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -5,12 +5,12 @@ env
 pwd
 
 brew update
-brew install qt@5
-brew unlink qt@5 && brew link --force qt@5
+brew install qt
+
 #brew link --overwrite qt@5 --force --overwrite
 # https://github.com/Homebrew/homebrew-core/issues/8392
 # https://github.com/Homebrew/legacy-homebrew/issues/29938
-QT_VERSION=$(brew list --versions qt@5 | awk '{print $2}')
+QT_VERSION=$(brew list --versions qt | awk '{print $2}')
 echo "QT_VERSION=$QT_VERSION"
 if [[ -z "$QT_VERSION" ]]; then
     echo "QT_VERSION is empty"
@@ -27,12 +27,12 @@ source ~/.bash_profile
 uname -a
 if [[ $(uname -m) == 'arm64' ]]; then
   echo "Run on MacOS arm64"
-  sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
-  sudo ln -s -F "/opt/homebrew/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
+  sudo ln -s -F "/opt/homebrew/Cellar/qt/${QT_VERSION}/mkspecs" /usr/local/mkspecs
+  sudo ln -s -F "/opt/homebrew/Cellar/qt/${QT_VERSION}/plugins" /usr/local/plugins
 else
   echo "Run on MacOS x86"
-  sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/mkspecs" /usr/local/mkspecs
-  sudo ln -s "/usr/local/Cellar/qt@5/${QT_VERSION}/plugins" /usr/local/plugins
+  sudo ln -s "/usr/local/Cellar/qt/${QT_VERSION}/mkspecs" /usr/local/mkspecs
+  sudo ln -s "/usr/local/Cellar/qt/${QT_VERSION}/plugins" /usr/local/plugins
 fi
 
 brew install ninja tree
