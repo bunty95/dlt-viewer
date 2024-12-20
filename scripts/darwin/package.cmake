f34386e699d0432884da72fc8f458e0fceba8c28
bunty95 <146869867+bunty95@users.noreply.github.com>
2024-12-20 16:56:51 -0500

    Update package.cmake

--- scripts/darwin/package.cmake
+++ scripts/darwin/package.cmake
@@ -5,8 +5,13 @@ endif()
 # See build.sh and src/cmake/Darwin.cmake
 set(CPACK_GENERATOR External)
 
-get_target_property(MOC_LOCATION ${QT_PREFIX}::Core LOCATION)
-get_filename_component(MACDEPLOYQT_EXECUTABLE ${MOC_LOCATION}/../../../bin/macdeployqt ABSOLUTE)
+get_target_property(qmake_executable Qt${QT_VERSION_MAJOR}::qmake IMPORTED_LOCATION)
+get_filename_component(qt_bin_dir "${qmake_executable}" DIRECTORY)
+find_program(MACDEPLOYQT_EXECUTABLE macdeployqt HINTS "${qt_bin_dir}")
+
+# get_target_property(MOC_LOCATION ${QT_PREFIX}::Core LOCATION)
+# get_filename_component(MACDEPLOYQT_EXECUTABLE ${MOC_LOCATION}/../../../bin/macdeployqt ABSOLUTE)
+
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/scripts/darwin/macdeployqt.cmake.in" "${CMAKE_BINARY_DIR}/macdeployqt.cmake" @ONLY)
 
 set(CPACK_EXTERNAL_PACKAGE_SCRIPT "${CMAKE_BINARY_DIR}/macdeployqt.cmake")
